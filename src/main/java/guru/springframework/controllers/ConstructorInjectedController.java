fb9cb45fbe1fda304da132b7473040f62553c557
Antonio Flores Pena <antonio.flores@4thsource.com>
2019-02-05 10:29:04 -0600

    set Autowired and Controller annotations

--- src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
@@ -2,6 +2,7 @@ package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingService;
 import guru.springframework.services.GreetingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by jt on 5/24/17.
@@ -10,6 +11,7 @@ public class ConstructorInjectedController {
 
     private GreetingService greetingService;
 
+    @Autowired
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
